986e355f9fa4b2c6cc364b92122f5a9cfff94ac4
shenlinnn <slinda1992@gmail.com>
2017-03-26 23:29:19 +0800

    Q2: keys pressed

--- main.py
+++ main.py
@@ -15,8 +15,21 @@ def count_key_press(input):
         n_press += index * count
     return n_press
 
-# print count_key_press('hello')
-# 13
-# print count_key_press('wonderful')
-# 20
+print count_key_press('hello')
+#13
+print count_key_press('wonderful')
+#20
 
+def number_pressed(input):
+    key_pressed = ''
+    for char in input:
+        for key, value in keypad.iteritems():
+            if char in value:
+                key_pressed += key
+                break
+    return int(key_pressed)
+
+print number_pressed('hello')
+#43556
+print number_pressed('wonderful')
+#966337385
